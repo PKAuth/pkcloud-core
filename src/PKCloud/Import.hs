f9862cce97b333c237f031c3e2eadddadcf8b736
James Parker <dev@jamesparker.me>
2015-11-17 16:16:00 -0500

    Export lifted getCurrentTime for apps

--- src/PKCloud/Import.hs
+++ src/PKCloud/Import.hs
@@ -1,10 +1,11 @@
 -- | PKCloud applications should typically import this module. 
 
-module PKCloud.Import (module Export) where
+module PKCloud.Import (module Export, getCurrentTime) where
 
 import Data.Int as Export
 import Data.Text as Export (Text)
 import Data.Time.Clock as Export (UTCTime)
+import qualified Data.Time.Clock as Time
 import Database.Esqueleto as Export
 -- import Database.Persist.Class as Export (EntityField)
 import Yesod.Auth as Export
@@ -15,4 +16,8 @@ import Yesod.Form.Bootstrap3 as Export
 
 import PKCloud.Core as Export
 import PKCloud.Security as Export
+
+-- | Data.Time.Clock's `getCurrentTime` lifted for convenience.
+getCurrentTime :: (MonadIO m) => m UTCTime
+getCurrentTime = liftIO Time.getCurrentTime
 
