e5420f871f980e17b5f78569fb79967c4fd8da25
James Parker <dev@jamesparker.me>
2018-09-25 13:37:24 -0400

    helper functions withAutocapitalizeNone, withAutocorrectOff

--- src/PKCloud/Internal.hs
+++ src/PKCloud/Internal.hs
@@ -10,8 +10,8 @@ import qualified Data.Time.Clock as Time
 import Database.Esqueleto as Export hiding (Value)
 import Text.Shakespeare.I18N (RenderMessage)
 import Yesod.Core (whamlet, HandlerSite, PathPiece)
-import Yesod.Form.Fields (checkBoxField)
-import Yesod.Form.Types (Field(..), FormMessage)
+import Yesod.Form.Fields (checkBoxField) -- , OptionList, radioField, optionsPairs)
+import Yesod.Form.Types (Field(..), FormMessage, FieldSettings(..))
 
 -- | Type constraint for subsite persistent entities for convenience. 
 type SubEntity e = (PersistEntity e, SqlBackend ~ PersistEntityBackend e, PathPiece (Key e), ToJSON (Key e), FromJSON (Key e), ToBackendKey SqlBackend e)
@@ -22,6 +22,7 @@ getCurrentTime :: (MonadIO m) => m Time.UTCTime
 getCurrentTime = liftIO Time.getCurrentTime
 
 
+-- JP: Make a PKCloud.Form module?
 -- https://gist.github.com/carymrobbins/590515bb8dfb48573527
 bootstrapCheckBoxField :: (Monad m, RenderMessage (HandlerSite m) FormMessage) => Text -> Field m Bool
 bootstrapCheckBoxField label = checkBoxField
@@ -35,4 +36,30 @@ bootstrapCheckBoxField label = checkBoxField
   where
     showVal = either $ const False
     -- style="margin-top: -20px; margin-bottom: 0">
+
+withAutocapitalizeNone :: FieldSettings site -> FieldSettings site
+withAutocapitalizeNone fs = fs { fsAttrs = newAttrs }
+    where newAttrs = ("autocapitalize", "none") : fsAttrs fs
+
+withAutocorrectOff :: FieldSettings site -> FieldSettings site
+withAutocorrectOff fs = fs { fsAttrs = newAttrs }
+    where newAttrs = ("autocorrect", "off") : fsAttrs fs
+
+-- bootstrapRadioField :: (Eq a, RenderMessage site FormMessage) => HandlerT site IO (OptionList a) -> Field (HandlerT site IO) a
+-- bootstrapRadioField l = selectFieldHelper undefined undefined undefined l
+-- 
+-- (radioField l)
+--     { fieldView = \theId name attrs val req -> do
+--         opts <- fmap olOptions $ handlerToWidget l
+--         [whamlet|
+--             $newline never
+--             <div .radio>
+--                 <label>
+--                     <input id=#{theId} *{attrs} type="checkbox" name=#{name} value=yes :showVal id val:checked> #{label}
+--             TODO
+--         |]
+--     }
+-- 
+-- bootstrapRadioFieldList :: (Eq a, RenderMessage site FormMessage, RenderMessage site msg) => [(msg, a)] -> Field (HandlerT site IO) a
+-- bootstrapRadioFieldList l = bootstrapRadioField . optionsPairs
 
